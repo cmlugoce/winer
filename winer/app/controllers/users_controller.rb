857f237976129b5c55ed3f0836d68914835de316
cmlugoce <cmlugoce@syr.edu>
2018-08-06 13:03:28 -0700

    add methods

--- winer/app/controllers/users_controller.rb
+++ winer/app/controllers/users_controller.rb
@@ -1,19 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id 
+      redirect_to root_path
+
+    else
+      render :new 
+    end 
+  end 
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
-  def update
-  end
 
-  def edit
-  end
+  private 
 
-  def delete
-  end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end 
 end
